5911e7afacf91478425908af524f52acdae0e267
4NdY-69 <adani.69@icloud.com>
2020-03-30 23:36:00 +0400

    4 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,13 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   TheSlider: any[];
+  play = 0;
+  change_it(){
+    // document.querySelectorAll('#slider .continer')[this.play].style.right = 0;
+    // console.log(this.play)
+    console.log(document.querySelectorAll('#slider .continer')[this.play].style.right);
+    // if(this.play == 3){
+    //   this.play = 0
+    // }else{this.play += 1}
+  };
+  
   constructor() {
     this.TheSlider = [1,2,3,4,5];
-    
+    // setInterval(this.change_it, 100, "my text");
   }
 
   ngOnInit(): void {
-    
+    // setInterval(this.change_it, 1000, "my text");
   }
-
 }
